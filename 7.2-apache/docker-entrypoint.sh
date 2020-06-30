27c9fe565cd2d5360191f27b98fe2d55e1e392d1
Pavel <picnixer@gmail.com>
2020-06-30 12:32:31 +0500

    deprecated version of php-fpm image

--- 7.2-apache/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Sourcing Apache env vars"
-source /etc/apache2/envvars
-echo "Apply $APACHE_RUN_USER:$APACHE_RUN_GROUP ownership to /var/www directory tree"
-chown -R "$APACHE_RUN_USER:$APACHE_RUN_GROUP" /var/www
-
-echo "Starting Apache"
-exec apache2-foreground
